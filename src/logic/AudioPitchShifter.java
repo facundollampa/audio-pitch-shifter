0f2b620e1e85df232e4d0dae4df4e10875182ffb
Facundo Llampa <a.facundollampa@gmail.com>
2019-12-05 23:37:01 -0300

    implemented oscilloscope .

--- src/logic/AudioPitchShifter.java
+++ src/logic/AudioPitchShifter.java
@@ -11,6 +11,8 @@ import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.AudioProcessor;
 import be.tarsos.dsp.GainProcessor;
+import be.tarsos.dsp.Oscilloscope;
+import be.tarsos.dsp.Oscilloscope.OscilloscopeEventHandler;
 import be.tarsos.dsp.WaveformSimilarityBasedOverlapAdd;
 import be.tarsos.dsp.WaveformSimilarityBasedOverlapAdd.Parameters;
 import be.tarsos.dsp.io.jvm.AudioPlayer;
@@ -18,115 +20,114 @@ import be.tarsos.dsp.io.jvm.JVMAudioInputStream;
 import be.tarsos.dsp.resample.RateTransposer;
 
 public class AudioPitchShifter {
-	
-	/**
-	 * 
-	 */
-	private AudioDispatcher dispatcher;
-	private WaveformSimilarityBasedOverlapAdd wsola;
-	private GainProcessor gain;
-	private AudioPlayer audioPlayer;
-	private RateTransposer rateTransposer;
-	private AudioFormat format;
-	private static double currentFactor;// pitch shift factor
-	private double gainValue;
-	private double sampleRate;
 
-	public AudioPitchShifter() {
-		currentFactor = 1;
-		gainValue = 1;
-		
-	}
-	
-	public void initiate() {
-		
-		if(dispatcher != null){
-			dispatcher.stop();
-		}
-		
-		try {
-			
-			setParameters();				
-			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-			
-			if(!AudioSystem.isLineSupported(info)) {
-				System.out.println("line not supported");
-			}
-			
-			final TargetDataLine targetline = (TargetDataLine) AudioSystem.getLine(info);
-			targetline.open(format);
-			targetline.start();
-			final AudioInputStream stream = new AudioInputStream(targetline);//antes estaba line
-			JVMAudioInputStream audioStream = new JVMAudioInputStream(stream);
-			
-			startDispatcher(audioStream);
-			
-			Thread audioProcessor = new Thread(dispatcher);
-			audioProcessor.start();
-		} 
-		catch (LineUnavailableException e) {e.printStackTrace();}	
-	}
-	
-	private void setParameters() {
-		format = new AudioFormat(44100, 16, 1, true,true);
-		rateTransposer = new RateTransposer(currentFactor);
-		gain = new GainProcessor(1.0);
-		try {
-			audioPlayer = new AudioPlayer(format);
-		} catch (LineUnavailableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		sampleRate = format.getSampleRate();
-		wsola = new WaveformSimilarityBasedOverlapAdd(Parameters.musicDefaults(currentFactor, sampleRate));	
-	}
-	
-	private void startDispatcher(JVMAudioInputStream audioStream) {
-		// create a new dispatcher
-		dispatcher = new AudioDispatcher(audioStream, wsola.getInputBufferSize(),wsola.getOverlap()); 
-		
-		wsola.setDispatcher(dispatcher);
-		dispatcher.addAudioProcessor(wsola);
-		dispatcher.addAudioProcessor(rateTransposer);
-		dispatcher.addAudioProcessor(gain);
-		dispatcher.addAudioProcessor(audioPlayer);
-		dispatcher.addAudioProcessor(new AudioProcessor() {
-			
-		@Override
-		public void processingFinished() {
-			if(true){//era loop antes
-			dispatcher =null;
-			initiate();
-			}	
-		}
-		@Override
-			public boolean process(AudioEvent audioEvent) {
-				return true;
-			}
-		});
-	}
-	
-	public void changeGain(double newGainValue) {
-		gainValue = newGainValue;
-		gain.setGain(gainValue);
-	}
-	
-	public double getGain() {
-		return gainValue;
-	}
-	
-	public void changeCurrentFactor(double newCurrentFactor) {
-		currentFactor = newCurrentFactor;
-		wsola.setParameters(WaveformSimilarityBasedOverlapAdd.Parameters.musicDefaults(currentFactor, sampleRate));
-		rateTransposer.setFactor(currentFactor);
-	}
-	public double getCurrentFactor() {
-		return currentFactor;
-	}
-	
-	public boolean isDispatcherNull() {
-		return dispatcher == null;
-	}
-	
+    private AudioDispatcher dispatcher;
+    private WaveformSimilarityBasedOverlapAdd wsola;
+    private GainProcessor gain;
+    private AudioPlayer audioPlayer;
+    private RateTransposer rateTransposer;
+    private AudioFormat format;
+
+    private static double currentFactor;
+    private double gainValue;
+    private double sampleRate;
+    private OscilloscopeEventHandler asss;
+
+    public AudioPitchShifter(OscilloscopeEventHandler h) {
+        currentFactor = 1;
+        gainValue = 1;
+        asss = h;
+    }
+
+    public void initiate() {
+
+        if(dispatcher != null){
+            dispatcher.stop();
+        }
+        try {
+            setParameters();
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            if(!AudioSystem.isLineSupported(info)) {
+            System.out.println("line not supported");
+            }
+
+            final TargetDataLine targetline = (TargetDataLine) AudioSystem.getLine(info);
+            targetline.open(format,4096);
+            targetline.start();
+            final AudioInputStream stream = new AudioInputStream(targetline);//antes estaba line
+            JVMAudioInputStream audioStream = new JVMAudioInputStream(stream);
+
+            startDispatcher(audioStream);
+
+            Thread audioProcessor = new Thread(dispatcher);
+            audioProcessor.start();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();}
+        }
+
+    private void setParameters() {
+        format = new AudioFormat(44100, 16, 1, true,true);
+        rateTransposer = new RateTransposer(currentFactor);
+        gain = new GainProcessor(1.0);
+        try {
+            audioPlayer = new AudioPlayer(format);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        sampleRate = format.getSampleRate();
+        wsola = new WaveformSimilarityBasedOverlapAdd(Parameters.musicDefaults(currentFactor, sampleRate));
+    }
+
+    private void startDispatcher(JVMAudioInputStream audioStream) {
+        // create a new dispatcher
+        dispatcher = new AudioDispatcher(audioStream, wsola.getInputBufferSize(),wsola.getOverlap());
+
+        wsola.setDispatcher(dispatcher);
+        dispatcher.addAudioProcessor(wsola);
+        dispatcher.addAudioProcessor(rateTransposer);
+        dispatcher.addAudioProcessor(gain);
+        dispatcher.addAudioProcessor(audioPlayer);
+        dispatcher.addAudioProcessor(new Oscilloscope(asss));
+        dispatcher.addAudioProcessor(new AudioProcessor() {
+
+            @Override
+            public void processingFinished() {
+                if(true){//era loop antes
+                    dispatcher =null;
+                    initiate();
+                }
+            }
+            @Override
+            public boolean process(AudioEvent audioEvent) {
+                return true;
+            }
+        });
+    }
+
+    public void changeGain(double newGainValue) {
+        gainValue = newGainValue;
+        gain.setGain(gainValue);
+    }
+
+    public double getGain() {
+        return gainValue;
+    }
+
+    public void changeCurrentFactor(double newCurrentFactor) {
+        currentFactor = newCurrentFactor;
+        wsola.setParameters(WaveformSimilarityBasedOverlapAdd.Parameters.musicDefaults(currentFactor, sampleRate));
+        rateTransposer.setFactor(currentFactor);
+    }
+    public double getCurrentFactor() {
+        return currentFactor;
+    }
+
+    public boolean isDispatcherNull() {
+        return dispatcher == null;
+    }
+    
+    public void setos(OscilloscopeEventHandler as) {
+    	asss = as;
+    }
 
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.lang.reflect.InvocationTargetException;
 
@@ -14,118 +17,98 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.Oscilloscope.OscilloscopeEventHandler;
 import logic.AudioPitchShifter;
 
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame implements OscilloscopeEventHandler{
 
-	private static final long serialVersionUID = 1L;
-	
-	private final JSlider factorSlider;
-	private final JLabel pitch_factorLabel;
-	private final JLabel gain_factorLabel;
-	private final JSlider gainSlider;
-	private static AudioPitchShifter audioPitchShifter = new AudioPitchShifter();
-	
-	public static void main(String[] argv) {
-		
-		audioPitchShifter.initiate();
-		try {	
-			startGui();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			throw new Error(e);
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-			throw new Error(e);
-		}
-	}
-	
-	public static void startGui() throws InterruptedException, InvocationTargetException{
-		SwingUtilities.invokeAndWait(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-				} catch (Exception e) {
-					//ignore failure to set default look en feel;
-				}
-				JFrame frame = new MainFrame();
-				frame.pack();
-				frame.setLocationRelativeTo(null); 
-				frame.setSize(400,200);//tamaño de la ventana completa
-				frame.setVisible(true);
-			}
-		});
-	}
-	
-	public MainFrame() {
-		
-		this.setLayout(new BorderLayout());
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Audio Pitch Shifter");
-		
-		
-		
-		factorSlider = new JSlider(20, 250);							//para
-		factorSlider.setValue(100);										//controlar
-		factorSlider.setPaintLabels(true);								//la 
-		factorSlider.addChangeListener(parameterSettingChangedListener);//frecuencia
-		
-		JPanel inputSubPanel = new JPanel(new BorderLayout());
-		
-		gainSlider = new JSlider(0,200);
-		gainSlider.setValue(100);
-		gainSlider.setPaintLabels(true);
-		gainSlider.addChangeListener(parameterSettingGain);
-		
-		JPanel params = new JPanel(new BorderLayout());
-		params.setBorder(new TitledBorder("1. Change frecuency value"));
-		JPanel params_individual = new JPanel(new GridLayout(0,3));
-		JLabel label_agudo = new JLabel("High Voice Pitch");
-		JLabel label_normal = new JLabel("Normal Voice Pitch");
-		JLabel label_grave = new JLabel("Low Voice Pitch");
-		params_individual.add(label_agudo);
-		params_individual.add(label_normal);
-		params_individual.add(label_grave);
-		pitch_factorLabel = new JLabel("Factor 100%");
-		pitch_factorLabel.setToolTipText("Factor de frecuncia en % (100 es normal, 50 es el doble de frecuencia y 200 es la mitad)");
-		params.add(pitch_factorLabel,BorderLayout.NORTH);
-		params.add(params_individual,BorderLayout.CENTER);
-		params.add(factorSlider,BorderLayout.SOUTH);
-		
-		JPanel gainPanel = new JPanel(new BorderLayout());
-		gain_factorLabel = new JLabel("Gain - Factor 100%");
-		gain_factorLabel.setToolTipText("Volume in % (normal value is 100)");
-		gainPanel.add(gain_factorLabel,BorderLayout.NORTH);
-		gainPanel.add(gainSlider,BorderLayout.CENTER);
-		gainPanel.setBorder(new TitledBorder("2. Volume"));
-		
-		//aqui esta ej JFrame de la ventana completa
-		this.add(inputSubPanel,BorderLayout.NORTH);
-		this.add(params,BorderLayout.CENTER);
-		this.add(gainPanel,BorderLayout.SOUTH);
-	}
-	
-	private ChangeListener parameterSettingGain = new ChangeListener() {
-		@Override
-		public void stateChanged(ChangeEvent arg0) {
-			if (!audioPitchShifter.isDispatcherNull()) {
-				audioPitchShifter.changeGain(gainSlider.getValue() / 100.0);
-			}
-			gain_factorLabel.setText("Ganancia - Factor "+ Math.round(audioPitchShifter.getGain() * 100)+"%");
-		}
-	};
-	
-	private ChangeListener parameterSettingChangedListener = new ChangeListener(){
-		@Override
-		public void stateChanged(ChangeEvent arg0) {
-			if (arg0.getSource() instanceof JSlider) {
-				audioPitchShifter.changeCurrentFactor(factorSlider.getValue() / 100.0);
-			}
-			pitch_factorLabel.setText("Factor " + Math.round(audioPitchShifter.getCurrentFactor()* 100) + "%");
-		}};
-		
-		
+    private static final long serialVersionUID = 1L;
+    private final JSlider factorSlider;
+    private final JLabel pitch_factorLabel;
+    private final JLabel gain_factorLabel;
+    private final JSlider gainSlider;
+    private AudioPitchShifter audioPitchShifter = new AudioPitchShifter(this);
+    private final GraphPanel panel;
+
+    public static void main(String[] args) {
+        JFrame frame = new MainFrame();
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setSize(600,300);//tamaño de la ventana completa
+        frame.setVisible(true);
+    }
+
+
+    public MainFrame() {
+    	audioPitchShifter.initiate();
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Audio Pitch Shifter");
+
+        factorSlider = new JSlider(20, 250);
+        factorSlider.setValue(100);
+        factorSlider.setPaintLabels(true);
+        factorSlider.addChangeListener(parameterSettingChangedListener);
+
+        panel = new GraphPanel();
+        gainSlider = new JSlider(0,200);
+        gainSlider.setValue(100);
+        gainSlider.setPaintLabels(true);
+        gainSlider.addChangeListener(parameterSettingGain);
+
+        JPanel params = new JPanel(new BorderLayout());
+        params.setBorder(new TitledBorder("1. Change frecuency value"));
+        JPanel params_individual = new JPanel(new GridLayout(0,3));
+        JLabel label_agudo = new JLabel("High Voice Pitch");
+        JLabel label_normal = new JLabel("Normal Voice Pitch");
+        JLabel label_grave = new JLabel("Low Voice Pitch");
+        params_individual.add(label_agudo);
+        params_individual.add(label_normal);
+        params_individual.add(label_grave);
+        pitch_factorLabel = new JLabel("Factor 100%");
+        pitch_factorLabel.setToolTipText("Factor de frecuncia en % (100 es normal, 50 es el doble de frecuencia y 200 es la mitad)");
+        params.add(pitch_factorLabel,BorderLayout.NORTH);
+        params.add(params_individual,BorderLayout.CENTER);
+        params.add(factorSlider,BorderLayout.SOUTH);
+
+        JPanel gainPanel = new JPanel(new BorderLayout());
+        gain_factorLabel = new JLabel("Gain - Factor 100%");
+        gain_factorLabel.setToolTipText("Volume in % (normal value is 100)");
+        gainPanel.add(gain_factorLabel,BorderLayout.NORTH);
+        gainPanel.add(gainSlider,BorderLayout.CENTER);
+        gainPanel.setBorder(new TitledBorder("2. Volume"));
+
+        //aqui esta ej JFrame de la ventana completa
+        this.add(panel,BorderLayout.SOUTH);
+//        this.add(inputSubPanel,BorderLayout.NORTH);
+        this.add(params,BorderLayout.NORTH);
+        this.add(gainPanel,BorderLayout.CENTER);
+    }
+
+    private ChangeListener parameterSettingGain = new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent arg0) {
+            if (!audioPitchShifter.isDispatcherNull()) {
+                audioPitchShifter.changeGain(gainSlider.getValue() / 100.0);
+            }
+            gain_factorLabel.setText("Ganancia - Factor "+ Math.round(audioPitchShifter.getGain() * 100)+"%");
+        }
+    };
+
+    private ChangeListener parameterSettingChangedListener = new ChangeListener(){
+        @Override
+        public void stateChanged(ChangeEvent arg0) {
+            if (arg0.getSource() instanceof JSlider) {
+                audioPitchShifter.changeCurrentFactor(factorSlider.getValue() / 100.0);
+            }
+            pitch_factorLabel.setText("Factor " + Math.round(audioPitchShifter.getCurrentFactor()* 100) + "%");
+        }};
+    	
+    @Override
+    public void handleEvent(float[] data, AudioEvent event) {
+    	panel.paint(data,event);
+    	panel.repaint();
+    }
 }
